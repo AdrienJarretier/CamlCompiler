e985cc2280393469b857a9cc7507adb8e707bf70
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-10-13 22:20:45 +0200

    tests finally works

--- testClasses/ParserTests.cpp
+++ testClasses/ParserTests.cpp
@@ -10,3 +10,46 @@ void writeTestFile(const char *text) {
 void removeFile() {
     remove(pathFile.c_str());
 }
+
+void ParserTests::SetUp() {
+}
+
+void ParserTests::TearDown() {
+    removeFile();
+}
+
+
+TEST_F(ParserTests, CreateParse) {
+    ASSERT_NO_THROW(
+        Parser parser(PathTestCamlFile.c_str());
+    );
+}
+
+TEST_F(ParserTests, LoadFile) {
+    const char * content = "-2 * 3;;2+3*5;;-(5+1)*(-2+5)+2*3;;";
+    writeTestFile(content);
+    Parser parser(pathFile.c_str());
+    ASSERT_EQ(std::string(content), parser.getSrcContent().c_str());
+}
+
+TEST_F(ParserTests, NextPhrase) {
+    const char * content = "-2 * 3;;2+3*5;;-(5+1)*(-2+5)+2*3;;";
+    const std::string firstOperation = "-2 * 3";
+    const std::string secondOperation = "2+3*5";
+    writeTestFile(content);
+    Parser parser(pathFile.c_str());
+    ASSERT_EQ(firstOperation, parser.nextPhrase());
+    ASSERT_EQ(secondOperation, parser.nextPhrase());
+}
+
+TEST_F(ParserTests, RemoveContent) {
+    const char *text = "(* Ecrire une fonction pair qui a un entier n associe un booléen valant"
+                            "vrai ssi n est pair. *)"
+                            "let pair = fun n -> n mod 2 = 0;;";
+    writeTestFile(text);
+    Parser parser(pathFile.c_str());
+    const std::string expectedResult = "let pair = fun n -> n mod 2 = 0;;";
+    parser.removeComments();
+    ASSERT_EQ(expectedResult, parser.getSrcContent());
+}
+
--- testClasses/ParserTests.hpp
+++ testClasses/ParserTests.hpp
@@ -3,52 +3,17 @@
 
 #include <gtest/gtest.h>
 #include "Parser.hpp"
-
-typedef ::testing::Test ParserTests;
 const std::string pathFile = "testFiles/example.ml";
 const std::string PathTestCamlFile = "testFiles/srcTest.ml";
 
+class ParserTests : public testing::Test {
+public:
+    virtual void SetUp();
+    virtual void TearDown();
+};
 void writeTestFile(const char *text);
 
 void removeFile();
 
-
-TEST_F(ParserTests, CreateParse) {
-    ASSERT_NO_THROW(
-        Parser parser(PathTestCamlFile.c_str());
-        removeFile();
-    );
-}
-
-TEST_F(ParserTests, LoadFile) {
-    const char * content = "-2 * 3;;2+3*5;;-(5+1)*(-2+5)+2*3;;";
-    writeTestFile(content);
-    Parser parser(pathFile.c_str());
-    ASSERT_EQ(std::string(content), parser.getSrcContent().c_str());
-    removeFile();
-}
-
-TEST_F(ParserTests, NextPhrase) {
-    const char * content = "-2 * 3;;2+3*5;;-(5+1)*(-2+5)+2*3;;";
-    const std::string firstOperation = "-2 * 3";
-    const std::string secondOperation = "2+3*5";
-    writeTestFile(content);
-    Parser parser(pathFile.c_str());
-    ASSERT_EQ(firstOperation, parser.nextPhrase());
-    ASSERT_EQ(secondOperation, parser.nextPhrase());
-    removeFile();
-}
-
-TEST_F(ParserTests, RemoveContent) {
-    const char *text = "(* Ecrire une fonction pair qui a un entier n associe un booléen valant"
-                            "vrai ssi n est pair. *)"
-                            "let pair = fun n -> n mod 2 = 0;;";
-    writeTestFile(text);
-    Parser parser(pathFile.c_str());
-    const std::string expectedResult = "let pair = fun n -> n mod 2 = 0;;";
-    parser.removeComments();
-    ASSERT_EQ(expectedResult, parser.getSrcContent());
-    removeFile();
-}
 
 #endif // PARSERTESTS_HPP
