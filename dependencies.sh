a912dbc04f3572ac21fc113d3039e87e26f85be7
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-12-04 14:12:10 +0100

    update deployment

--- dependencies.sh
+++ dependencies.sh
@@ -7,20 +7,6 @@ if [ "$(uname)" == "Darwin" ]; then
   cmake -Dgtest_build_samples=ON -Dgtest_build_tests=ON ../.
   make clean
   make all
-
-  echo "works line1"
-  sudo mkdir /usr/local/Cellar/gtest
-  
-  echo "works line2"
-  sudo cp gtest-1.8.0/build/libgtest.a /usr/local/Cellar/gtest/
-  
-  echo "works line3"
-  sudo ln -snf /usr/local/Cellar/gtest/libgtest.a /usr/local/lib/libgtest.a
-  
-  echo "works line4"
-  sudo cp -r gtest-1.8.0/include /usr/local/Cellar/gtest/
-  ln -snf ../Cellar/gtest/include/gtest /usr/local/include/gtest
-
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   # Do something under GNU/Linux platform
   sudo apt-get install libgtest-dev
