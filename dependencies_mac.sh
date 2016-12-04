79dd88360e44bcb074e168dcb642a7831f15c9a4
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-12-04 22:11:47 +0100

    split script

---
+++ dependencies_mac.sh
@@ -0,0 +1,9 @@
+# Do something under Mac OS X platform
+echo "Mac OS"
+brew install cmake
+cd externalLib/gtest-1.8.0/googletest
+mkdir build
+cd build
+cmake -Dgtest_build_samples=ON -Dgtest_build_tests=ON ../.
+make clean
+make all
