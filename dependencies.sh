5ea527a3d9bbd2acff36fc724589be127f0bf0aa
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-12-04 11:17:30 +0100

    wip build in mac

--- dependencies.sh
+++ dependencies.sh
@@ -4,16 +4,23 @@ if [ "$(uname)" == "Darwin" ]; then
   cd externalLib/gtest-1.8.0/googletest
   mkdir build
   cd build
-  cmake ../
-  make
+  cmake -Dgtest_build_samples=ON -Dgtest_build_tests=ON ../.
+  make clean
+  make all
 
+  echo "works line1"
   sudo mkdir /usr/local/Cellar/gtest
-  cd ../../
-  sudo cp googletest/build/libgtest.a /usr/local/Cellar/gtest/
+  
+  echo "works line2"
+  sudo cp gtest-1.8.0/build/libgtest.a /usr/local/Cellar/gtest/
+  
+  echo "works line3"
   sudo ln -snf /usr/local/Cellar/gtest/libgtest.a /usr/local/lib/libgtest.a
-  #sudo ln -snf /usr/local/Cellar/gtest/libgtest.a /usr/lib/libgtest.a
-  sudo cp -r googletest/include /usr/local/Cellar/gtest/
+  
+  echo "works line4"
+  sudo cp -r gtest-1.8.0/include /usr/local/Cellar/gtest/
   ln -snf ../Cellar/gtest/include/gtest /usr/local/include/gtest
+
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   # Do something under GNU/Linux platform
   sudo apt-get install libgtest-dev
