01ebfe8a8ea12adea7a2ed830e6c221e7b8de4ff
Gomez Guillaume <guillaume.gomez846@gmail.com>
2016-12-04 22:05:41 +0100

    wip rollback

--- dependencies.sh
+++ dependencies.sh
@@ -1,22 +1,7 @@
-OS=`uname -s`
-if [  OS = "Darwin" ]; then
-  # Do something under Mac OS X platform
-  echo "Mac OS"
-  brew install cmake
-  cd externalLib/gtest-1.8.0/googletest
-  mkdir build
-  cd build
-  cmake -Dgtest_build_samples=ON -Dgtest_build_tests=ON ../.
-  make clean
-  make all
-elif [  OS = "Linux" ]; then
-  echo "Linux"
-  # Do something under GNU/Linux platform
-  sudo apt-get install libgtest-dev
-  sudo apt-get install cmake # install cmake
-  cd /usr/src/gtest
-  sudo cmake CMakeLists.txt
-  sudo make clean
-  sudo make all
-  sudo cp *.a /usr/lib
-fi
+sudo apt-get install libgtest-dev
+sudo apt-get install cmake # install cmake
+cd /usr/src/gtest
+sudo cmake CMakeLists.txt
+sudo make clean
+sudo make all
+sudo cp *.a /usr/lib
